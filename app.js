16efeba2048b33c570e65042c39cd0d083272d11
sofikin <sofia.kalpin@khk.ee>
2021-12-03 09:52:37 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function weatherDataFetch(city){
         }) // Convert data to json
         .then(function(data) {
             console.log(data);
+            drawWeather(data);
         })
         .catch(function (){
             // catch any errors
@@ -16,4 +17,13 @@ function weatherDataFetch(city){
 
 function cityWeather(e){
     weatherDataFetch("Tallinn");
+}
+
+function drawWeather(data){
+    var celcius = Math.round(parseFloat(data.main.temp) -273.15);
+    var description  = data.weather[0].description;
+
+    document.querySelector("#description").innerHTML = description;
+    document.querySelector("#temp").innerHTML = celcius + "&deg;";
+    document.querySelector("#location").innerHTML = data.name;
 }
